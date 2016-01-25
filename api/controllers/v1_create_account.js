a24768b70a2e7712e780e34e3b484d71281487f5
Sami Pyy <sami_pyy@fastmail.fm>
2016-01-25 16:15:51 +0000

    ...

--- api/controllers/v1_create_account.js
+++ api/controllers/v1_create_account.js
@@ -3,56 +3,6 @@
 var https = require('https');
 
 
-
-function saasPost(request, response) {
-  var postData = JSON.stringify({
-    'user': 'user1', 
-    'server': 'eesee.io', 
-    'password': 'user1'
-    
-  });
-  var options = { 
-    hostname: 'eesee0-test.m.in-app.io',
-    port: 5281, 
-    path: '/api/create_account',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': postData.length
-    }
-  };  
-
-  var saasRequest = https.request(options, function(saasResponse) {
-    
-    console.log('statusCode: ', saasResponse.statusCode);
-    console.log('headers: ', saasResponse.headers);
-    
-    var body = ''; 
-
-    saasResponse.on('data', function(chunk) {
-      console.log('saasResponse.on: data');
-      body += chunk;
-    }); 
-
-    saasResponse.on('end', function() {
-      console.log('saasResponse.on: end');
-      response.statusCode = saasResponse.statusCode;
-      response.setHeader('Content-Type', 'application/json');
-      //res.writeHead(saasResponse.statusCode, saasResponse.headers);
-      response.end(body);
-    });
-    
-  });
-  
-  saasRequest.write(postData);
-  saasRequest.end();
-  
-  saasRequest.on('error', function(e) {
-    console.error(e);
-  });
-}
-
-
 function post(request, response) {
   var userDetails = request.swagger.params.V1UserDetails.value;
   var body = JSON.stringify(userDetails);
@@ -68,19 +18,15 @@ function post(request, response) {
   };  
 
   var saasRequest = https.request(options, function(saasResponse) {
-    
     console.log('statusCode: ', saasResponse.statusCode);
-    console.log('headers: ', saasResponse.headers);
     
     var body = ''; 
 
     saasResponse.on('data', function(chunk) {
-      console.log('saasResponse.on: data');
       body += chunk;
     }); 
 
     saasResponse.on('end', function() {
-      console.log('saasResponse.on: end');
       response.setHeader('Content-Type', 'application/json');
       response.end(body);
     });
--- api/controllers/v1_delete_account.js
+++ api/controllers/v1_delete_account.js
@@ -6,12 +6,8 @@ var https = require('https');
 
 
 function post(request, response) {
-  var postData = JSON.stringify({
-    'user': 'user1', 
-    'server': 'eesee.io', 
-    'password': 'user1'
-    
-  });
+  var userDetails = request.swagger.params.V1UserDetails.value;
+  var body = JSON.stringify(userDetails);
   var options = { 
     hostname: 'eesee0-test.m.in-app.io',
     port: 5281, 
@@ -19,33 +15,28 @@ function post(request, response) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Content-Length': postData.length
+      'Content-Length': body.length
     }
   };  
 
   var saasRequest = https.request(options, function(saasResponse) {
     
     console.log('statusCode: ', saasResponse.statusCode);
-    console.log('headers: ', saasResponse.headers);
     
     var body = ''; 
 
     saasResponse.on('data', function(chunk) {
-      console.log('saasResponse.on: data');
       body += chunk;
     }); 
 
     saasResponse.on('end', function() {
-      console.log('saasResponse.on: end');
-      response.statusCode = saasResponse.statusCode;
       response.setHeader('Content-Type', 'application/json');
       response.end(body);
     });
     
   });
   
-  saasRequest.write(postData);
-  saasRequest.end();
+  saasRequest.end(body);
   
   saasRequest.on('error', function(e) {
     console.error(e);
